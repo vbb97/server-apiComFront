f2068a4be4a458cc1b5c4a5c5f7acc8a2bc3daae
Vitor Bueno <vbbueno97@gmail.com>
2023-03-11 14:39:34 -0300

    criando rota inicial

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ import userRoutes from './userRoutes';
 import loginRoutes from './loginRoutes';
 import contactRoutes from './contactRoutes';
 
+router.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Tudo funcionado.' });
+});
 router.use('/users', userRoutes);
 router.use('/login', loginRoutes);
 router.use('/contacts', contactRoutes);
